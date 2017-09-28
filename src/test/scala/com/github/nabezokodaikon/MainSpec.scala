e9b0b596fbe4ff653879bef6fdf2ec76011d3858
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-09-28 11:52:21 +0900

    Clean format on code

--- src/test/scala/com/github/nabezokodaikon/MainSpec.scala
+++ src/test/scala/com/github/nabezokodaikon/MainSpec.scala
@@ -1,9 +1,9 @@
 package com.github.nabezokodaikon
 
-import org.scalatest.FunSuite                       
+import org.scalatest.FunSuite
 
-class MainSpec extends FunSuite {                   
-  test("helloWorld") {                              
+class MainSpec extends FunSuite {
+  test("helloWorld") {
     assert(Main.helloWorld("nabezokodaikokn") == "Hello nabezokodaikokn!")
-  }                                                 
-}                                                   
+  }
+}
