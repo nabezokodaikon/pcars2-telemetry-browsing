20bee79c0aa7ee32e5ac418108dcc8e6ab7a28e1
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-01 23:48:31 +0900

    Modify actor control

--- src/main/scala/com/github/nabezokodaikon/Main.scala
+++ src/main/scala/com/github/nabezokodaikon/Main.scala
@@ -1,12 +1,15 @@
 package com.github.nabezokodaikon
 
-import akka.actor.ActorSystem
+import akka.actor.{ ActorSystem, Props }
 import akka.http.scaladsl.Http
+import akka.pattern.{ gracefulStop, AskTimeoutException }
 import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.LazyLogging
 import scala.concurrent.duration._
-// import scala.concurrent.Await
+import scala.concurrent.Await
 import scala.io.StdIn
+
+object ActorDone
 
 object Main extends App with LazyLogging {
   implicit val system = ActorSystem("pcars2-udp-app")
@@ -18,9 +21,21 @@ object Main extends App with LazyLogging {
 
   logger.info(helloWorld("nabezokodaikokn"))
 
+  val udpProps = Props(classOf[UdpListener])
+  val udpListener = system.actorOf(udpProps, "udpListener")
+
   val httpRoute = HttpServer.route
   Http().bindAndHandle(httpRoute, "192.168.1.18", 9000)
   println("Started server at 192.168.1.18:9000, press enter to stop server")
   StdIn.readLine()
+
+  try {
+    val stopped = gracefulStop(udpListener, 5 seconds, ActorDone)
+    Await.result(stopped, 6 seconds)
+  } catch {
+    case e: AskTimeoutException =>
+      logger.error(e.getMessage)
+  }
+
   system.terminate()
 }
--- src/main/scala/com/github/nabezokodaikon/UdpListener.scala
+++ src/main/scala/com/github/nabezokodaikon/UdpListener.scala
@@ -1,12 +1,22 @@
 package com.github.nabezokodaikon
 
-import akka.actor.Actor
+import akka.actor.{ Actor, Terminated }
 import com.typesafe.scalalogging.LazyLogging
 
-object UdpListener extends Actor with LazyLogging {
+class UdpListener extends Actor with LazyLogging {
 
   def receive = {
+    case text: String =>
+      println(text)
+    case ActorDone =>
+      println("Done.")
+      context.stop(self)
     case _ =>
+      println
       logger.warn("Received unknown message.")
+  }
+
+  override def postStop = {
+    println("postStop")
   }
 }
