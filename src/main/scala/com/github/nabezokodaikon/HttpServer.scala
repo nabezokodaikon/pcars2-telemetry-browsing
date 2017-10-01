239f1ae86725490d990aca25adf1105b8615e187
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-01 21:11:46 +0900

    Modify Http Server to resource read only

--- src/main/scala/com/github/nabezokodaikon/HttpServer.scala
+++ src/main/scala/com/github/nabezokodaikon/HttpServer.scala
@@ -1,7 +1,54 @@
 package com.github.nabezokodaikon
 
+import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+// import akka.NotUsed
+// import akka.stream.scaladsl.{ Flow, Sink, Source }
+import com.github.nabezokodaikon.util.FileUtil
 import com.typesafe.scalalogging.LazyLogging
 
 object HttpServer extends LazyLogging {
 
+  private val resourceDirectory = {
+    val current = FileUtil.getCurrentDirectory
+    s"${current}/resource"
+  }
+
+  // def httpResource(file: String) {
+  // val source = Source.single(file)
+  // val flow = Flow[String]
+  // .map { f =>
+  // val contentType = FileUtil.getContentType(f)
+  // val text = FileUtil.readText(f)
+  // HttpEntity(contentType, text)
+  // }
+  // val sink = Sink.last[]()
+  // println(sink.toString)
+  // }
+
+  val route: Route =
+    pathSingleSlash {
+      get {
+        val file = s"${resourceDirectory}/index.html"
+        val contentType = FileUtil.getContentType(file)
+        val text = FileUtil.readText(file)
+        complete(HttpEntity(contentType, text))
+      }
+    } ~
+      // TODO
+      // path("api") {
+      // post {
+      // }
+      // }
+      // } ~
+      path(Segments) { x: List[String] =>
+        get {
+          val segments = x.mkString("/")
+          val file = s"${resourceDirectory}/${segments}"
+          val contentType = FileUtil.getContentType(file)
+          val text = FileUtil.readText(file)
+          complete(HttpEntity(contentType, text))
+        }
+      }
 }
--- src/main/scala/com/github/nabezokodaikon/Main.scala
+++ src/main/scala/com/github/nabezokodaikon/Main.scala
@@ -1,12 +1,26 @@
 package com.github.nabezokodaikon
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
 import com.typesafe.scalalogging.LazyLogging
+import scala.concurrent.duration._
+// import scala.concurrent.Await
+import scala.io.StdIn
 
 object Main extends App with LazyLogging {
+  implicit val system = ActorSystem("pcars2-udp-app")
+  implicit val materializer = ActorMaterializer()
 
   def helloWorld(name: String): String = {
     "Hello " + name + "!"
   }
 
   logger.info(helloWorld("nabezokodaikokn"))
+
+  val httpRoute = HttpServer.route
+  Http().bindAndHandle(httpRoute, "192.168.1.18", 9000)
+  println("Started server at 192.168.1.18:9000, press enter to stop server")
+  StdIn.readLine()
+  system.terminate()
 }
