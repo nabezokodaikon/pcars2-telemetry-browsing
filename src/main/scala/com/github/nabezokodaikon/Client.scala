767ac1fa470b00296206d51cc00591e64bd2436b
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-02 18:35:08 +0900

    Modify client implementation

--- src/main/scala/com/github/nabezokodaikon/Client.scala
+++ src/main/scala/com/github/nabezokodaikon/Client.scala
@@ -1,44 +1,78 @@
 package com.github.nabezokodaikon
 
 import akka.actor.{ Actor, ActorRef, Props }
-import akka.pattern.{ AskTimeoutException, ask }
+import akka.pattern.{ AskTimeoutException, ask, gracefulStop }
 import com.typesafe.scalalogging.LazyLogging
+import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{ Failure, Success }
 
-object Client {
+object ClientManager {
   case class AddClient(client: ActorRef)
   case class RemoveClient(client: ActorRef)
+}
+
+class ClientManager extends Actor with LazyLogging {
+  import ClientManager._
+
+  def receive = processing(List[ActorRef]())
+
+  private def processing(clientList: List[ActorRef]): Receive = {
+    case AddClient(client) =>
+      context.become(processing(clientList :+ client))
+    case RemoveClient(client) => {
+      try {
+        val stopped = gracefulStop(client, 5.seconds, ActorDone)
+        Await.result(stopped, 6.seconds)
+      } catch {
+        case e: AskTimeoutException =>
+          logger.error(e.getMessage)
+      } finally {
+        context.become(processing(clientList.filter(_ != client).toList))
+      }
+    }
+    case UdpListener.OutgoingValue(value) =>
+      val msg = UdpListener.OutgoingValue(value)
+      clientList.foreach(_ ! msg)
+    case ActorDone =>
+      println("ClientManager Done.")
+      context.stop(self)
+    case _ =>
+      logger.warn("Received unknown message.")
+  }
+
+  override def postStop = {
+    println("UdpListener stop.")
+  }
+}
+
+object Client {
   case class Connected(outgoing: ActorRef)
-  case class OutgoingValue(value: String)
 }
 
-class HttpClientManager {
-}
-
-class Client(udpListener: ActorRef) extends Actor with LazyLogging {
+class Client(manager: ActorRef) extends Actor with LazyLogging {
   import Client._
 
   def receive = {
     case Connected(outgoing) =>
-      udpListener ! UdpListener.AddClient(self)
+      manager ! ClientManager.AddClient(self)
       context.become(processing(outgoing))
     case _ =>
       logger.warn("Received unknown message.")
   }
 
   private def processing(outgoing: ActorRef): Receive = {
-    case OutgoingValue(value) =>
+    case UdpListener.OutgoingValue(value) =>
       val f = (outgoing ? value)(5.seconds).mapTo[String]
       f.onComplete {
         case Success(_) => Unit
         case Failure(e) =>
           logger.info(e.getMessage)
-          udpListener ! UdpListener.RemoveClient(self)
+          manager ! ClientManager.RemoveClient(self)
       }
     case ActorDone =>
-      println("Done.")
+      println("Client Done.")
       context.stop(self)
     case _ =>
       logger.warn("Received unknown message.")
--- src/main/scala/com/github/nabezokodaikon/UdpListener.scala
+++ src/main/scala/com/github/nabezokodaikon/UdpListener.scala
@@ -7,37 +7,36 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 
 object UdpListener {
-  case class AddClient(client: ActorRef)
-  case class RemoveClient(client: ActorRef)
+  case class OutgoingValue(value: String)
 }
 
-class UdpListener extends Actor with LazyLogging {
-  import UdpListener._
+// class UdpListener extends Actor with LazyLogging {
+// import UdpListener._
 
-  def receive = processing(List[ActorRef]())
+// def receive = processing(List[ActorRef]())
 
-  private def processing(clientList: List[ActorRef]): Receive = {
-    case AddClient(client) =>
-      context.become(processing(clientList :+ client))
-    case RemoveClient(client) => {
-      try {
-        val stopped = gracefulStop(client, 5.seconds, ActorDone)
-        Await.result(stopped, 6.seconds)
-      } catch {
-        case e: AskTimeoutException =>
-          logger.error(e.getMessage)
-      } finally {
-        context.become(processing(clientList.filter(_ != client).toList))
-      }
-    }
-    case ActorDone =>
-      println("Done.")
-      context.stop(self)
-    case _ =>
-      logger.warn("Received unknown message.")
-  }
+// private def processing(clientList: List[ActorRef]): Receive = {
+// case AddClient(client) =>
+// context.become(processing(clientList :+ client))
+// case RemoveClient(client) => {
+// try {
+// val stopped = gracefulStop(client, 5.seconds, ActorDone)
+// Await.result(stopped, 6.seconds)
+// } catch {
+// case e: AskTimeoutException =>
+// logger.error(e.getMessage)
+// } finally {
+// context.become(processing(clientList.filter(_ != client).toList))
+// }
+// }
+// case ActorDone =>
+// println("Done.")
+// context.stop(self)
+// case _ =>
+// logger.warn("Received unknown message.")
+// }
 
-  override def postStop = {
-    println("UdpListener stop.")
-  }
-}
+// override def postStop = {
+// println("UdpListener stop.")
+// }
+// }
