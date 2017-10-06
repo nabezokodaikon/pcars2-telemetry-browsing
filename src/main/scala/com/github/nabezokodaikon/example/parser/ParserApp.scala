d7ccc214444758c20d8d0aac2afe863b54c0eedf
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-06 16:49:02 +0900

    Temp commit

--- src/main/scala/com/github/nabezokodaikon/example/parser/ParserApp.scala
+++ src/main/scala/com/github/nabezokodaikon/example/parser/ParserApp.scala
@@ -1,7 +1,7 @@
 package com.github.nabezokodaikon.example
 
 import ch.ethz.acl.passera.unsigned._
-import com.github.nabezokodaikon.util.BinaryUtil
+import com.github.nabezokodaikon.util.{ BinaryUtil, SharedMemoryConstants }
 import com.github.nabezokodaikon.util.FileUtil
 import com.typesafe.scalalogging.LazyLogging
 
@@ -10,13 +10,60 @@ object ParserApp extends App with LazyLogging {
   val dir = FileUtil.getCurrentDirectory()
   val name = s"${dir}/testdata/20171005120800046.bin"
   var mVersionData = FileUtil.readBinary(name).toList
-  println(mVersionData.toString)
+  // println(mVersionData.toString)
 
-  val (mVersion, mBuildVersionNumberData) = BinaryUtil.readInt(mVersionData)
-  // println(mVersion.toString)
-  // println(String.valueOf(mVersion).toCharArray())
+  // Version Number
+  val (mVersion, mBuildVersionNumberData) = BinaryUtil.readInt(mVersionData) match {
+    case Some((v, d)) => (v, d)
+    case None => (0, Nil)
+  }
+  println(mVersion.toString)
 
-  val (mBuildVersionNumber, mGameStateData) = BinaryUtil.readInt(mBuildVersionNumberData)
-  // println(mBuildVersionNumber.toString)
-  // println(String.valueOf(mBuildVersionNumber).toCharArray())
+  val (mBuildVersionNumber, mGameStateData) = BinaryUtil.readInt(mBuildVersionNumberData) match {
+    case Some((v, d)) => (v, d)
+    case None => (0, Nil)
+  }
+  println(mBuildVersionNumber.toString)
+
+  // Game States
+  val (mGameState, mSessionStateData) = BinaryUtil.readInt(mGameStateData) match {
+    case Some((v, d)) => (v, d)
+    case None => (0, Nil)
+  }
+  println(mGameState.toString)
+
+  val (mSessionState, mRaceStateData) = BinaryUtil.readInt(mSessionStateData) match {
+    case Some((v, d)) => (v, d)
+    case None => (0, Nil)
+  }
+  println(mSessionState.toString)
+
+  val (mRaceState, mViewedParticipantIndexData) = BinaryUtil.readInt(mRaceStateData) match {
+    case Some((v, d)) => (v, d)
+    case None => (0, Nil)
+  }
+  println(mRaceState.toString)
+
+  // Participant Info
+  val (mViewedParticipantIndex, mNumParticipantsData) = BinaryUtil.readInt(mViewedParticipantIndexData) match {
+    case Some((v, d)) => (v, d)
+    case None => (0, Nil)
+  }
+  println(mViewedParticipantIndex.toString)
+
+  val (mNumParticipants, mIsActiveData) = BinaryUtil.readInt(mNumParticipantsData) match {
+    case Some((v, d)) => (v, d)
+    case None => (0, Nil)
+  }
+  println(mNumParticipants.toString)
+
+  // ParticipantInfo
+  val (mIsActive, mNameData) = BinaryUtil.readBoolean(mIsActiveData) match {
+    case Some((v, d)) => (v, d)
+    case None => (false, Nil)
+  }
+  println(mIsActive.toString)
+
+  val (mName, mWorldPositionData) = BinaryUtil.readCharArray(mNameData, SharedMemoryConstants.STRING_LENGTH_MAX)
+  println(mName)
 }
--- src/main/scala/com/github/nabezokodaikon/util/BinaryUtil.scala
+++ src/main/scala/com/github/nabezokodaikon/util/BinaryUtil.scala
@@ -4,23 +4,81 @@ import ch.ethz.acl.passera.unsigned._
 import com.typesafe.scalalogging.LazyLogging
 import java.nio.ByteBuffer
 
+object SharedMemoryConstants {
+  val STRING_LENGTH_MAX: Int = 64
+}
+
 object BinaryUtil extends LazyLogging {
 
-  def readUInt(data: List[Byte]): (UInt, List[Byte]) =
+  // public boolean readBoolean() throws IOException {
+
+  // int bool = in.read();
+  // if (bool == -1) throw new EOFException();
+  // return (bool != 0);
+
+  // }
+
+  // public char readChar() throws IOException {
+
+  // int byte1 = in.read();
+  // int byte2 = in.read();
+  // if (byte2 == -1) throw new EOFException();
+  // return (char) (((byte2 << 24) >>> 16) + ((byte1 << 24) >>> 24));
+
+  // }
+
+  def readChar(data: List[Byte]): Option[(Char, List[Byte])] = {
     data match {
-      case a :: b :: c :: d :: tail =>
-        (ByteBuffer.wrap(Array(a, b, c, d)).getInt.toUInt, tail)
-      case _ => (UInt.MinValue, Nil)
+      case byte1 :: byte2 :: tail =>
+        if (byte2 == -1) {
+          None
+        } else {
+          Some(((((byte2 << 24) >>> 16) + ((byte1 << 24) >>> 24)).toChar, tail))
+        }
+      case _ => None
+    }
+  }
+
+  def readCharArray(data: List[Byte], stringLength: Int): (List[Char], List[Byte]) = {
+
+    def go(currentChars: List[Char], currentData: List[Byte], currentCount: Int): List[Char] = {
+      readChar(currentData) match {
+        case Some((nextChar, nextData)) =>
+          if (currentCount < 1) {
+            currentChars
+          } else {
+            val nextChars = currentChars :+ nextChar
+            val nextCount = currentCount - 1
+            go(nextChars, nextData, nextCount)
+          }
+        case None => currentChars
+      }
     }
 
-  def readInt(data: List[Byte]): (Int, List[Byte]) =
+    (go(List[Char](), data, stringLength), data.drop(stringLength * 2))
+  }
+
+  def readBoolean(data: List[Byte]): Option[(Boolean, List[Byte])] = {
     data match {
-      case a :: b :: c :: d :: tail =>
-        // val s = new String(Array(a, b, c, d), "US-ASCII")
-        // println(s)
-        val t = String.valueOf(Array(a, b, c, d))
-        println(t)
-        (ByteBuffer.wrap(Array(a, b, c, d)).getInt, tail)
-      case _ => (' ', Nil)
+      case byte :: tail =>
+        if (byte == -1) {
+          None
+        }
+        Some((byte != 0, tail))
+      case _ => None
+    }
+  }
+
+  def readInt(data: List[Byte]): Option[(Int, List[Byte])] =
+    data match {
+      case byte1 :: byte2 :: byte3 :: byte4 :: tail =>
+        if (byte4 == -1) {
+          None
+        }
+        Some(((byte4 << 24)
+          + ((byte3 << 24) >>> 8)
+          + ((byte2 << 24) >>> 16)
+          + ((byte1 << 24) >>> 24), tail))
+      case _ => None
     }
 }
