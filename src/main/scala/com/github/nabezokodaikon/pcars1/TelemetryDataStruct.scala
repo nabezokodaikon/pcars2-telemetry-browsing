3cc1fbe99582b93ad85d3a70d7d26834d8517dba
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-08 19:28:37 +0900

    Add telemetry data structs

---
+++ src/main/scala/com/github/nabezokodaikon/pcars1/TelemetryDataStruct.scala
@@ -0,0 +1,162 @@
+package com.github.nabezokodaikon.pcars1
+
+// 64 Byte
+case class NameString(
+  nameByteArray: Array[Int] // 64
+)
+
+// 1,347 Byte
+case class ParticipantInfoStrings(
+  buildVersionNumber: Int,
+  packetType: Int,
+  carName: Array[Int], // 64
+  carClassName: Array[Int], // 64
+  trackLocation: Array[Int], // 64
+  trackVariation: Array[Int], // 64
+  name: Array[NameString] // 16
+)
+
+// 1,028 Byte
+case class ParticipantInfoStringsAdditional(
+  buildVersionNumber: Int,
+  packetType: Int,
+  offset: Int,
+  name: Array[NameString] // 16
+)
+
+// 16 Byte
+case class ParticipantInfo(
+  worldPosition: Array[Short], // 3
+  currentLapDistance: Int,
+  racePosition: Int,
+  lapsCompleted: Int,
+  currentLap: Int,
+  sector: Int,
+  lastSectorTime: Float)
+
+// 1,367 Byte
+case class TelemetryData(
+  buildVersionNumber: Int,
+  packetType: Int,
+
+  // Game states
+  gameSessionState: Int,
+
+  // Participant info
+  viewedParticipantIndex: Byte,
+  numParticipants: Byte,
+
+  // Unfiltered input
+  unfilteredThrottle: Int,
+  unfilteredBrake: Int,
+  unfilteredSteering: Byte,
+  unfilteredClutch: Int,
+  raceStateFlags: Int,
+
+  // Event information
+  lapsInEvent: Int,
+
+  // Timings
+  bestLapTime: Float,
+  lastLapTime: Float,
+  currentTime: Float,
+  splitTimeAhead: Float,
+  splitTimeBehind: Float,
+  splitTime: Float,
+  eventTimeRemaining: Float,
+  personalFastestLapTime: Float,
+  worldFastestLapTime: Float,
+  currentSector1Time: Float,
+  currentSector2Time: Float,
+  currentSector3Time: Float,
+  fastestSector1Time: Float,
+  fastestSector2Time: Float,
+  fastestSector3Time: Float,
+  personalFastestSector1Time: Float,
+  personalFastestSector2Time: Float,
+  personalFastestSector3Time: Float,
+  worldFastestSector1Time: Float,
+  worldFastestSector2Time: Float,
+  worldFastestSector3Time: Float,
+  joyPad1: Int,
+  joyPad2: Int,
+
+  // Flags
+  highestFlag: Int,
+
+  // Pit info
+  pitModeSchedule: Int,
+
+  // Car state
+  oilTempCelsius: Short,
+  oilPressureKPa: Int,
+  waterTempCelsius: Short,
+  waterPressureKpa: Int,
+  fuelPressureKpa: Short,
+  carFlags: Int,
+  fuelCapacity: Int,
+  brake: Int,
+  throttle: Int,
+  clutch: Int,
+  steering: Byte,
+  fuelLevel: Float,
+  speed: Float,
+  rpm: Int,
+  maxRpm: Int,
+  gearNumGears: Int,
+  boostAmount: Int,
+  enforcedPitStopLap: Byte,
+  crashState: Int,
+  odometerKM: Float,
+  orientation: Array[Float], // 3
+  localVelocity: Array[Float], // 3
+  worldVelocity: Array[Float], // 3
+  angularVelocity: Array[Float], // 3
+  localAcceleration: Array[Float], // 3
+  worldAcceleration: Array[Float], // 3
+  extentsCentre: Array[Float], // 3
+
+  // Wheels / Tyres
+  tyreFlags: Array[Int], // 4
+  terrain: Array[Int], // 4
+  tyreY: Array[Float], // 4
+  tyreRPS: Array[Float], // 4
+  tyreSlipSpeed: Array[Float], // 4
+  tyreTemp: Array[Int], // 4
+  tyreGrip: Array[Int], // 4
+  tyreHeightAboveGround: Array[Float], // 4
+  tyreLateralStiffness: Array[Float], // 4
+  tyreWear: Array[Int], // 4
+  brakeDamage: Array[Int], // 4
+  suspensionDamage: Array[Int], // 4
+  brakeTempCelsius: Array[Short], // 4
+  tyreTreadTemp: Array[Int], // 4
+  tyreLayerTemp: Array[Int], // 4
+  tyreCarcassTemp: Array[Int], // 4
+  tyreRimTemp: Array[Int], // 4
+  tyreInternalAirTemp: Array[Int], // 4
+  wheelLocalPositionY: Array[Float], // 4
+  rideHeight: Array[Float], // 4
+  suspensionTravel: Array[Float], // 4
+  suspensionVelocity: Array[Float], // 4
+  airPressure: Array[Int], // 4
+
+  // Extras
+  engineSpeed: Float,
+  engineTorque: Float,
+
+  // Car damage
+  aeroDamage: Int,
+  engineDamage: Int,
+
+  // Weather
+  ambientTemperature: Byte,
+  trackTemperature: Byte,
+  rainDensity: Int,
+  windSpeed: Byte,
+  windDirectionX: Byte,
+  windDirectionY: Byte,
+  participantInfo: Array[ParticipantInfo], // 56
+  trackLength: Float,
+  wings: Array[Int], // 2
+  dPad: Int)
