7ed9870ae5a55648b334c42d8b0e99204bbeb052
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-08 21:32:47 +0900

    Add  const define of telemetry data

---
+++ src/main/scala/com/github/nabezokodaikon/pcars1/TelemetryDataConst.scala
@@ -0,0 +1,14 @@
+package com.github.nabezokodaikon.pcars1
+
+object TelemetryDataConst {
+
+  // Data size
+  val TELEMETRY_DATA_SIZE: Int = 1367
+  val PARTICIPANT_INFO_STRINGS_SIZE: Int = 1347
+  val PARTICIPANT_INFO_STRINGS_ADDITIONAL_SIZE: Int = 1028
+
+  // Frame type
+  val TELEMETRY_DATA_FRAME_TYPE: Int = 0
+  val PARTICIPANT_INFO_STRINGS_FRAME_TYPE: Int = 1
+  val PARTICIPANT_INFO_STRINGS_ADDITIONAL_FRAME_TYPE: Int = 2
+}
