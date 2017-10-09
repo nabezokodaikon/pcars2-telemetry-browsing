0b729e62c9d0ae8e81cca0a81053d851d7e50d97
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-09 12:48:26 +0900

    Temp commit

--- src/main/scala/com/github/nabezokodaikon/example/parser/ParserApp.scala
+++ src/main/scala/com/github/nabezokodaikon/example/parser/ParserApp.scala
@@ -1,9 +1,11 @@
 package com.github.nabezokodaikon.example
 
+import com.github.nabezokodaikon.pcars1._
 import com.github.nabezokodaikon.pcars1.BinaryUtil._
 import com.github.nabezokodaikon.pcars1.Encoding
 import com.github.nabezokodaikon.pcars1.ParticipantInfo
 import com.github.nabezokodaikon.pcars1.SharedMemoryConstants._
+import com.github.nabezokodaikon.pcars1.TelemetryDataStructFactory._
 import com.github.nabezokodaikon.util.FileUtil
 import com.typesafe.scalalogging.LazyLogging
 
@@ -11,7 +13,7 @@ object ParserApp extends App with LazyLogging {
 
   val dir = FileUtil.getCurrentDirectory()
   val name = s"${dir}/testdata/1_20171008215718126.bin"
-  var pcarsData = FileUtil.readBinary(name).toList
-  // println(pcarsData.length)
-  // println(pcarsData.toString)
+  var data = FileUtil.readBinary(name).toList
+  println(data.length)
+  // println(data.toString)
 }
