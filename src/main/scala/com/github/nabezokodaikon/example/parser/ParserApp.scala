490e8fb2ab9839a9ef3f06a4823709f7f74bbb9d
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-09 16:22:12 +0900

    Add Add process time measurement

--- src/main/scala/com/github/nabezokodaikon/example/parser/ParserApp.scala
+++ src/main/scala/com/github/nabezokodaikon/example/parser/ParserApp.scala
@@ -12,8 +12,43 @@ import com.typesafe.scalalogging.LazyLogging
 object ParserApp extends App with LazyLogging {
 
   val dir = FileUtil.getCurrentDirectory()
-  val name = s"${dir}/testdata/1_20171008215718126.bin"
-  var data = FileUtil.readBinary(name).toList
-  println(data.length)
-  // println(data.toString)
+
+  val telemetryDataName = s"${dir}/testdata/0_20171007151455850.bin"
+  var telemetryDataData = FileUtil.readBinary(telemetryDataName).toList
+  val telemetryData = createTelemetryData(telemetryDataData)
+  println(telemetryData.brake)
+  println(telemetryData.throttle)
+  println(telemetryData.clutch)
+  println(telemetryData.steering)
+  println(telemetryData.speed)
+  println(telemetryData.gearNumGears)
+
+  {
+    val start = System.currentTimeMillis
+    for (i <- 0 to 60) {
+      createTelemetryData(telemetryDataData)
+    }
+    val interval = System.currentTimeMillis - start
+    println(s"${interval} msec")
+  }
+
+  println("////////////////////////////////")
+
+  val participantInfoStringsName = s"${dir}/testdata/1_20171008215718126.bin"
+  var participantInfoStringsData = FileUtil.readBinary(participantInfoStringsName).toList
+  val participantInfoStrings = createParticipantInfoStrings(participantInfoStringsData)
+  println(participantInfoStrings.carName)
+  println(participantInfoStrings.carClassName)
+  println(participantInfoStrings.trackLocation)
+  println(participantInfoStrings.trackVariation)
+  println(participantInfoStrings.nameString(0))
+
+  {
+    val start = System.currentTimeMillis
+    for (i <- 0 to 60) {
+      createParticipantInfoStrings(participantInfoStringsData)
+    }
+    val interval = System.currentTimeMillis - start
+    println(s"${interval} msec")
+  }
 }
