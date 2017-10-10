8b678b4ffffb334b187a282b4fbb036a98215a7a
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-10 19:10:08 +0900

    Add   Convert to JSON 実装

--- src/main/scala/com/github/nabezokodaikon/example/parser/ParserApp.scala
+++ src/main/scala/com/github/nabezokodaikon/example/parser/ParserApp.scala
@@ -1,5 +1,6 @@
 package com.github.nabezokodaikon.example
 
+import akka.http.scaladsl.model.ws.TextMessage
 import com.github.nabezokodaikon.pcars1._
 import com.github.nabezokodaikon.pcars1.BinaryUtil._
 import com.github.nabezokodaikon.pcars1.Encoding
@@ -8,13 +9,13 @@ import com.github.nabezokodaikon.pcars1.TelemetryDataStructFactory._
 import com.github.nabezokodaikon.util.FileUtil
 import com.typesafe.scalalogging.LazyLogging
 
-object ParserApp extends /* App with */ LazyLogging {
+object ParserApp extends App with LazyLogging {
 
   val dir = FileUtil.getCurrentDirectory()
 
-  val telemetryDataName = s"${dir}/testdata/0_20171009190539027.bin"
-  var telemetryDataData = FileUtil.readBinary(telemetryDataName).toList
-  val telemetryData = createTelemetryData(telemetryDataData)
+  // val telemetryDataName = s"${dir}/testdata/0_20171009190539027.bin"
+  // var telemetryDataData = FileUtil.readBinary(telemetryDataName).toList
+  // val telemetryData = createTelemetryData(telemetryDataData)
   // println(telemetryData.brake)
   // println(telemetryData.throttle)
   // println(telemetryData.clutch)
@@ -33,21 +34,25 @@ object ParserApp extends /* App with */ LazyLogging {
 
   // println("////////////////////////////////")
 
-  // val participantInfoStringsName = s"${dir}/testdata/1_20171008215718126.bin"
-  // var participantInfoStringsData = FileUtil.readBinary(participantInfoStringsName).toList
-  // val participantInfoStrings = createParticipantInfoStrings(participantInfoStringsData)
+  val participantInfoStringsName = s"${dir}/testdata/1_20171008215718126.bin"
+  var participantInfoStringsData = FileUtil.readBinary(participantInfoStringsName).toList
+  val participantInfoStrings = createParticipantInfoStrings(participantInfoStringsData)
   // println(participantInfoStrings.carName)
   // println(participantInfoStrings.carClassName)
   // println(participantInfoStrings.trackLocation)
   // println(participantInfoStrings.trackVariation)
   // println(participantInfoStrings.nameString(0))
+  // println(participantInfoStrings.toJson)
+  // println(participantInfoStrings.toJson.toString)
 
-  // {
-  // val start = System.currentTimeMillis
-  // for (i <- 0 to 60) {
-  // createParticipantInfoStrings(participantInfoStringsData)
-  // }
-  // val interval = System.currentTimeMillis - start
-  // println(s"${interval} msec")
-  // }
+  {
+    val start = System.currentTimeMillis
+    for (i <- 0 to 60) {
+      val src = createParticipantInfoStrings(participantInfoStringsData)
+      val json = participantInfoStrings.toJsonString
+      val msg = TextMessage(json)
+    }
+    val interval = System.currentTimeMillis - start
+    println(s"${interval} msec")
+  }
 }
