af25b283ca651e13bca651efd131dac8e09cb51b
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-13 20:59:17 +0900

    Add telemetry const values

--- src/main/scala/com/github/nabezokodaikon/pcars1/TelemetryDataConst.scala
+++ src/main/scala/com/github/nabezokodaikon/pcars1/TelemetryDataConst.scala
@@ -11,4 +11,19 @@ object TelemetryDataConst {
   val TELEMETRY_DATA_FRAME_TYPE: Int = 0
   val PARTICIPANT_INFO_STRINGS_FRAME_TYPE: Int = 1
   val PARTICIPANT_INFO_STRINGS_ADDITIONAL_FRAME_TYPE: Int = 2
+
+  // TyreData and TyreUdpData index
+  val TYRE_FRONT_LEFT: Int = 0
+  val TYRE_FRONT_RIGHT: Int = 1
+  val TYRE_REAR_LEFT: Int = 2
+  val TYRE_REAR_RIGHT: Int = 3
+
+  // CarStateVecotrData index
+  val VEC_X: Int = 0
+  val VEC_Y: Int = 1
+  val VEC_Z: Int = 2
+
+  // Gear string
+  val GEAR_NEUTRAL: String = "N"
+  val GEAR_REVERS: String = "R"
 }
