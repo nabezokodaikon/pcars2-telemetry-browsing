582a2ce49a42abcf342cec5dcd1941a9ab4323c9
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-14 18:49:54 +0900

    Modify state to all telemetry data

--- public/src/car_state_data.js
+++ public/src/car_state_data.js
@@ -12,64 +12,18 @@ const RECEIVED_DATA = "RECEIVED_DATA";
 /*
  * action creators
  */
-function receivedData(data) {
+function receivedData(telemetry) {
   return { type: RECEIVED_DATA, telemetry };
 }
 
 const initialState = {
-  telemetry: {
-    oilTempCelsius: 0,
-    oilPressureKPa: 0,
-    waterTempCelsius: 0,
-    waterPressureKpa: 0,
-    fuelPressureKpa: 0,
-    carFlags: 0,
-    fuelCapacity: 0,
-    brake: 0.0,
-    throttle: 0.0,
-    clutch: 0.0,
-    steering: 0.0,
-    fuelLevel: 0.0,
-    speed: 0.0,
-    rpm: 0,
-    maxRpm: 0,
-    gear: 0,
-    numGears: 0,
-    boostAmount: 0,
-    enforcedPitStopLap: 0,
-    odometerKM: -1.0,
-    antiLockActive: false,
-    boostActive: false
-  }
+  telemetry: {}
 }
 
 function telemetry(state = {}, action) {
   switch (action.type) {
     case RECEIVED_DATA:
-      return {
-        oilTempCelsius: action.telemetry.carStateData.oilTempCelsius,
-        oilPressureKPa: action.telemetry.carStateData.oilPressureKPa,
-        waterTempCelsius: action.telemetry.carStateData.waterTempCelsius,
-        waterPressureKpa: action.telemetry.carStateData.waterPressureKpa,
-        fuelPressureKpa: action.telemetry.carStateData.fuelPressureKpa,
-        carFlags: action.telemetry.carStateData.carFlags,
-        fuelCapacity: action.telemetry.carStateData.fuelCapacity,
-        brake: action.telemetry.carStateData.brake,
-        throttle: action.telemetry.carStateData.throttle,
-        clutch: action.telemetry.carStateData.clutch,
-        steering: action.telemetry.carStateData.steering,
-        fuelLevel: action.telemetry.carStateData.fuelLevel,
-        speed: action.telemetry.carStateData.speed,
-        rpm: action.telemetry.carStateData.rpm,
-        maxRpm: action.telemetry.carStateData.maxRpm,
-        gear: action.telemetry.carStateData.gear,
-        numGears: action.telemetry.carStateData.numGears,
-        boostAmount: action.telemetry.carStateData.boostAmount,
-        enforcedPitStopLap: action.telemetry.carStateData.enforcedPitStopLap,
-        odometerKM: action.telemetry.carStateData.odometerKM,
-        antiLockActive: action.telemetry.carStateData.antiLockActive,
-        boostActive: action.telemetry.carStateData.boostActive
-      };
+      return action.telemetry;
     default:
       return state;
   }
