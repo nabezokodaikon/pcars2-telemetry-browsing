ef838880b270c127e9125d376a7d68bf628fc638
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-16 10:54:28 +0900

    Add Define contet names

---
+++ public/src/common/js/contentNames.js
@@ -0,0 +1,17 @@
+export const PARTICIPANT_INFO_STRINGS = "ParticipantInfoStrings";
+export const PARTICIPANT_INFO_STRINGS_ADDITIONAL = "ParticipantInfoStringsAdditional";
+export const GAMESTATE_DATA = "GameStateData";
+export const PARTICIPANT_INFO_DATA = "ParticipantInfoData";
+export const PARTICIPANT_INFO = "ParticipantInfo";
+export const UNFILTEREDINPUT_DATA = "UnfilteredInputData";
+export const EVENT_INFO_DATA = "EventInfoData";
+export const TIMING_INFO_DATA = "TimingInfoData";
+export const SECTOR_TIME_DATA = "SectorTimeData";
+export const FLAG_DATA = "FlagData";
+export const PITINFO_DATA = "PitInfoData";
+export const CAR_STATE_DATA = "CarStateData";
+export const CAR_STATE_VECOTRDATA = "CarStateVecotrData";
+export const TYRE_DATA = "TyreData";
+export const TYRE_UDP_DATA = "TyreUdpData";
+export const OTHER_UDP_DATA = "OtherUdpData";
+export const CAR_DAMAGE_DATA = "CarDamageData";
