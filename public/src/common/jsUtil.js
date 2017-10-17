575a2235d17e6e0e57ed9be78703dddf2f449986
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-17 22:01:19 +0900

    Add array judgment function

--- public/src/common/jsUtil.js
+++ public/src/common/jsUtil.js
@@ -2,6 +2,10 @@ export function isJson(obj) {
   return Object.keys(obj).length > 0;
 }
 
+export function isArray(array) {
+  return Array.isArray(array);
+}
+
 export function existsKey(obj, key) {
   return (key in obj)
 }
