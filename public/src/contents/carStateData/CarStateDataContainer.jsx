713c38ebcefc7c4714ecbaf58460b3ffca3232da
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-17 16:25:54 +0900

    Modify All car state data are display

--- public/src/contents/carStateData/CarStateDataContainer.jsx
+++ public/src/contents/carStateData/CarStateDataContainer.jsx
@@ -3,6 +3,7 @@ import ReactDom from "react-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { openWebSocket } from "../../appActionCreators.js";
+import { isTelemetryDataFrameType } from "../../common/telemetryUtil.js";
 
 class CarStateData extends React.Component {
   constructor(props) {
@@ -14,20 +15,35 @@ class CarStateData extends React.Component {
     this.props.onOpenWebSocket();
   }
 
-  renderSpeed() {
-    if (Object.keys(this.props.telemetry).length > 0 && this.props.telemetry.frameType == 0) {
-      return <p>{this.props.telemetry.carStateData.speed}</p>;
-    } else {
-      return <p>--:--:--.---</p>;
+  getData() {
+    if (!isTelemetryDataFrameType(this.props.telemetry)) {
+      return <div></div>;
     }
+
+    const createRecords = () => {
+      const data = this.props.telemetry.carStateData;
+      return Object.keys(data).map(key => {
+        const value = data[key];
+        return (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{value}</td>
+          </tr>
+        );
+      });
+    }
+
+    return (
+      <table>
+        <tbody>
+          {createRecords()}
+        </tbody>
+      </table>
+    );
   }
 
   render() {
-    return (
-      <div>
-        {this.renderSpeed()}
-      </div>
-    );
+    return this.getData();
   }
 }
 
