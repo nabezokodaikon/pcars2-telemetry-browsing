425aaac451bbe7958aaa967e96a1c8fe9dab398a
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-17 15:37:55 +0900

    Add telemetry const define

---
+++ public/src/common/telemetryConst.js
@@ -0,0 +1,15 @@
+// Frame type
+export const TELEMETRY_DATA_FRAME_TYPE = 0;
+export const PARTICIPANT_INFO_STRINGS_FRAME_TYPE = 1;
+export const PARTICIPANT_INFO_STRINGS_ADDITIONAL_FRAME_TYPE = 2;
+
+// TyreData and TyreUdpData index
+export const TYRE_FRONT_LEFT = 0;
+export const TYRE_FRONT_RIGHT = 1;
+export const TYRE_REAR_LEFT = 2;
+export const TYRE_REAR_RIGHT = 3;
+
+// CarStateVecotrData index
+export const VEC_X = 0;
+export const VEC_Y = 1;
+export const VEC_Z = 2;
