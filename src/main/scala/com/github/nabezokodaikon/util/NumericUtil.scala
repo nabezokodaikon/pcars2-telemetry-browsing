b65c57b10c868af30fb08f66391fbf1e5e9ff6f8
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-20 19:13:50 +0900

    Clean numeric util

--- src/main/scala/com/github/nabezokodaikon/util/NumericUtil.scala
+++ src/main/scala/com/github/nabezokodaikon/util/NumericUtil.scala
@@ -5,62 +5,74 @@ import java.math.{ BigDecimal, RoundingMode }
 object NumericUtil {
   import scala.language.implicitConversions
 
-  private def calcDivide(a: BigDecimal, b: BigDecimal, scale: Int): String =
-    if (scale > -1) a.divide(b, scale, RoundingMode.DOWN).toString
-    else a.divide(b, 0, RoundingMode.DOWN).toString
+  private def calcDivide(value: BigDecimal, divisor: BigDecimal, scale: Int): String =
+    if (scale > -1)
+      value.divide(divisor, scale, RoundingMode.DOWN).toString
+    else
+      value.divide(divisor, 0, RoundingMode.DOWN).toString
 
-  private def calcMultiply(a: BigDecimal, b: BigDecimal, scale: Int): String =
-    if (scale > -1) a.multiply(b).setScale(scale, RoundingMode.DOWN).toString
-    else a.multiply(b).setScale(0, RoundingMode.DOWN).toString
+  private def calcMultiply(value: BigDecimal, multiplicand: BigDecimal, scale: Int): String =
+    if (scale > -1)
+      value.multiply(multiplicand).setScale(scale, RoundingMode.DOWN).toString
+    else
+      value.multiply(multiplicand).setScale(0, RoundingMode.DOWN).toString
 
-  class IntRoundingSupport(value: Int) {
-    def divide(divisionValue: Int, scale: Int): String = {
-      val valueDecimal = new BigDecimal(value)
-      val divisionValueDecimal = new BigDecimal(divisionValue)
-      calcDivide(valueDecimal, divisionValueDecimal, scale)
-    }
+  trait RoundingSupport {
+    val value: BigDecimal
 
-    def divide(divisionValue: Float, scale: Int): String = {
-      val valueDecimal = new BigDecimal(value)
-      val divisionValueDecimal = new BigDecimal(divisionValue.toString)
-      calcDivide(valueDecimal, divisionValueDecimal, scale)
-    }
+    def divide(divisor: Byte, scale: Int): String =
+      calcDivide(value, new BigDecimal(divisor), scale)
+
+    def divide(divisor: Short, scale: Int): String =
+      calcDivide(value, new BigDecimal(divisor), scale)
+
+    def divide(divisor: Int, scale: Int): String =
+      calcDivide(value, new BigDecimal(divisor), scale)
+
+    def divide(divisor: Long, scale: Int): String =
+      calcDivide(value, new BigDecimal(divisor), scale)
+
+    def divide(divisor: Float, scale: Int): String =
+      calcDivide(value, new BigDecimal(divisor), scale)
+
+    def multiply(multiplicand: Float, scale: Int): String =
+      calcMultiply(value, new BigDecimal(multiplicand), scale)
   }
 
-  class LongRoundingSupport(value: Long) {
-    def divide(divisionValue: Int, scale: Int): String = {
-      val valueDecimal = new BigDecimal(value)
-      val divisionValueDecimal = new BigDecimal(divisionValue)
-      calcDivide(valueDecimal, divisionValueDecimal, scale)
-    }
-
-    def divide(divisionValue: Long, scale: Int): String = {
-      val valueDecimal = new BigDecimal(value)
-      val divisionValueDecimal = new BigDecimal(divisionValue)
-      calcDivide(valueDecimal, divisionValueDecimal, scale)
-    }
-
-    def divide(divisionValue: Float, scale: Int): String = {
-      val valueDecimal = new BigDecimal(value)
-      val divisionValueDecimal = new BigDecimal(divisionValue.toString)
-      calcDivide(valueDecimal, divisionValueDecimal, scale)
-    }
+  class ByteRoundingSupport(srcValue: Byte) extends RoundingSupport {
+    val value = new BigDecimal(srcValue)
   }
 
-  class FloatRoundingSupport(value: Float) {
-    def divide(divisionValue: Int, scale: Int): String = {
-      val valueDecimal = new BigDecimal(value.toString)
-      val divisionValueDecimal = new BigDecimal(divisionValue)
-      calcDivide(valueDecimal, divisionValueDecimal, scale)
-    }
-
-    def divide(divisionValue: Float, scale: Int): String = {
-      val valueDecimal = new BigDecimal(value.toString)
-      val divisionValueDecimal = new BigDecimal(divisionValue.toString)
-      calcDivide(valueDecimal, divisionValueDecimal, scale)
-    }
+  class ShortRoundingSupport(srcValue: Short) extends RoundingSupport {
+    val value = new BigDecimal(srcValue)
   }
 
+  class IntRoundingSupport(srcValue: Int) extends RoundingSupport {
+    val value = new BigDecimal(srcValue)
+  }
+
+  class LongRoundingSupport(srcValue: Long) extends RoundingSupport {
+    val value = new BigDecimal(srcValue)
+  }
+
+  class FloatRoundingSupport(srcValue: Float) extends RoundingSupport {
+    val value = new BigDecimal(srcValue)
+
+    def multiply(multiplicand: Byte, scale: Int): String =
+      calcMultiply(value, new BigDecimal(multiplicand), scale)
+
+    def multiply(multiplicand: Short, scale: Int): String =
+      calcMultiply(value, new BigDecimal(multiplicand), scale)
+
+    def multiply(multiplicand: Int, scale: Int): String =
+      calcMultiply(value, new BigDecimal(multiplicand), scale)
+
+    def multiply(multiplicand: Long, scale: Int): String =
+      calcMultiply(value, new BigDecimal(multiplicand), scale)
+  }
+
+  implicit def toBigDecimal(value: Byte) = new ByteRoundingSupport(value)
+  implicit def toBigDecimal(value: Short) = new ShortRoundingSupport(value)
   implicit def toBigDecimal(value: Int) = new IntRoundingSupport(value)
   implicit def toBigDecimal(value: Long) = new LongRoundingSupport(value)
   implicit def toBigDecimal(value: Float) = new FloatRoundingSupport(value)
