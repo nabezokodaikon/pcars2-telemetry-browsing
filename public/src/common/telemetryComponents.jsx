b057eaac7a33c10052889c81bd36d239fe4f670f
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-23 15:10:58 +0900

    Modify RPM component layout
    Added background on RPM.

--- public/src/common/telemetryComponents.jsx
+++ public/src/common/telemetryComponents.jsx
@@ -14,15 +14,23 @@ import {
 */
 export function createRpmComponent(rpm, maxRpm, cx, cy, radius, width) {
   const fontSize = width * 2;
-  const rpmValue = 300 / maxRpm * rpm + 30;
-  const separateDegree = 300 / maxRpm * 1000;
-  const maxRpmShape = createFanShape(cx, cy, radius, 30, 330, width, "#FF0000");
+  const allRpm = maxRpm * 1.17;
+  const rpmUnit = 300 / allRpm;
+  const rpmValue = rpmUnit * rpm + 30;
+  const maxRpmValue = rpmUnit * maxRpm + 30;
+  const separateDegree = 300 / allRpm * 1000;
+
+  const allRpmShape = createFanShape(cx, cy, radius, 30, 330, width, "#FF0000");
+  const maxRpmShape = createFanShape(cx, cy, radius, 30, maxRpmValue, width, "#000000");
   const rpmShape = createFanShape(cx, cy, radius, 30, rpmValue, width * 1.5, "#FFFFFF");
-  const rpmText = Array.from({length: Math.floor(maxRpm / 1000) + 1}, (v, k) => k).map(i =>
+
+  const rpmText = Array.from({length: Math.floor(allRpm / 1000) + 1}, (v, k) => k).map(i =>
     createFanText(cx, cy, radius - fontSize * 2, 30 + separateDegree * (i - 0.5), 30 + separateDegree * (i + 0.5), fontSize, "#5555AA", i.toString())
   );
+
   return (
     <g>
+      {allRpmShape}
       {maxRpmShape}
       {rpmShape}
       {rpmText}
