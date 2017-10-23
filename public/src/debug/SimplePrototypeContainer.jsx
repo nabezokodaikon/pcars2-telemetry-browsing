85ca13e6a89f61051d7b72162dae8f9f9e5478c4
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-23 11:37:40 +0900

    Modify Confirmed SVG define on react

--- public/src/debug/SimplePrototypeContainer.jsx
+++ public/src/debug/SimplePrototypeContainer.jsx
@@ -9,30 +9,60 @@ class SimpleProtoType extends React.Component {
     super(props)
   }
 
+  /*
+   * cx: 円の中心のx座標
+   * cy: 円の中心のy座標
+   * radius: 半径
+   * startDegree: 扇形の始まりの中心角(度数法)
+   * finishDegree: 扇形の終わりの中心角(度数法)
+   * width: 扇の幅
+   * strokeColor: 線の色
+   * strokeWidth: 線の幅
+  */
+  createFanStroke(cx, cy, radius, startDegree, finishDegree, width, strokeColor, strokeWidth) {
+    const startSinX = Math.sin(startDegree / 180 * Math.PI);
+    const startCosY = Math.cos(startDegree / 180 * Math.PI);
+    const finishSinX = Math.sin(finishDegree / 180 * Math.PI);
+    const finishCosY = Math.cos(finishDegree / 180 * Math.PI);
+
+    const innerRadius = radius - width;
+    const innerStartX = cx - innerRadius * startSinX;
+    const innerStartY = cy + innerRadius * startCosY;
+    const innerFinishX = cx - innerRadius * finishSinX;
+    const innerFinishY = cy + innerRadius * finishCosY;
+
+    const arcStartX = cx - radius * startSinX;
+    const arcStartY = cy + radius * startCosY;
+    const arcFinishX = cx - radius * finishSinX;
+    const arcFinishY = cy + radius * finishCosY;
+
+    const largeArcFlag = (finishDegree - startDegree <= 180) ? 0 : 1;
+
+    const startLine = "M" + innerStartX + " " + innerStartY + " L " + arcStartX + " " + arcStartY;
+    const arc = "A" + radius + " " + radius + " " + 0 + " " + largeArcFlag + " " + 1 + " " + arcFinishX + " " + arcFinishY;
+    const finishLine = "L" + innerFinishX + " " + innerFinishY + " ";
+
+    return (
+      <path
+        d={startLine + " " + arc + " " + finishLine}
+        fill="none"
+        stroke={strokeColor}
+        strokeWidth={strokeWidth}
+      />
+    );
+  }
+
   getData() {
     if (!isJson(this.props.telemetryData)) {
       return <div></div>;
     }
       
-    const createRecords = () => {
-      const data = this.props.telemetryData.carStateData;
-      return Object.keys(data).map(key => {
-        const value = data[key];
-        return (
-          <tr key={key}>
-            <td>{key}</td>
-            <td>{value}</td>
-          </tr>
-        );
-      });
-    }
+    const stroke = this.createFanStroke(500, 500, 400, 30, 330, 16, "#FF0000", 1);
 
     return (
-      <table>
-        <tbody>
-          {createRecords()}
-        </tbody>
-      </table>
+      <svg >
+        {stroke}
+      </svg>
     );
   }
 
