d474152dc5d0347a25092173abe6f0c2ac0a1ef5
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-10-26 12:12:17 +0900

    Modify prototype menu actions

--- public/prototype/menu.js
+++ public/prototype/menu.js
@@ -1,1 +1,36 @@
 console.log("Start.");
+
+window.addEventListener("load", evt => {
+  const open = document.getElementById("open");
+  const menu = document.getElementById("menu");
+  const filter = document.getElementById("filter");
+  const list = document.getElementById("list");
+
+  open.addEventListener("change", evt => {
+    if (open.checked) {
+      filter.style.visibility = "visible";
+    } else {
+      filter.style.visibility = "hidden";
+    }
+  });
+
+  Array.from(menu.getElementsByTagName("input")).forEach(elm => {
+    elm.addEventListener("click", evt => {
+      open.checked = false;
+      if (open.checked) {
+        filter.style.visibility = "visible";
+      } else {
+        filter.style.visibility = "hidden";
+      }
+    });
+  });
+
+  filter.addEventListener("click", evt => {
+    open.checked = false;
+    if (open.checked) {
+      filter.style.visibility = "visible";
+    } else {
+      filter.style.visibility = "hidden";
+    }
+  });
+});
