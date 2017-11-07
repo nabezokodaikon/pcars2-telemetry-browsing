17d526c9b252497ba57114ae1749622e11462497
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-11-08 06:44:47 +0900

    Clean

--- src/main/scala/com/github/nabezokodaikon/TimeDetailsStorage.scala
+++ src/main/scala/com/github/nabezokodaikon/TimeDetailsStorage.scala
@@ -67,8 +67,124 @@ final class TimeDetailsStorage(clientManager: ClientManager)
     logger.debug("TimeDetailsStorage postStop.")
   }
 
-  private def createHistory(viewedParticipantIndex: Byte) = {
+  def receive(): Receive = {
+    case udpData: TelemetryData =>
+      context.become(processing(createHistory(udpData.participantinfo.viewedParticipantIndex)))
+    case _ => Unit
+  }
+
+  def processing(history: History): Receive = {
+    case udpData: TelemetryData if udpData.participantinfo.viewedParticipantIndex != history.viewedParticipantIndex =>
+      val nextHistory = createHistory(udpData.participantinfo.viewedParticipantIndex)
+      context.become(processing(nextHistory))
+    case udpData: TimingsData =>
+      createHistory(udpData, history) match {
+        case Some(nextHistory) =>
+          context.become(processing(nextHistory))
+        case None => Unit
+      }
+    case udpData: TimeStatsData =>
+      createHistory(udpData, history) match {
+        case Some(nextHistory) =>
+          context.become(processing(nextHistory))
+        case None => Unit
+      }
+  }
+
+  private def createHistory(viewedParticipantIndex: Byte): History = {
     History(viewedParticipantIndex, None, None, None, List[LapData]())
+  }
+
+  def createHistory(udpData: TimingsData, history: History): Option[History] = {
+    history.currentData match {
+      case Some(currentData) =>
+        val participant = udpData.partcipants(history.viewedParticipantIndex)
+        if (participant.currentLap != currentData.currentLap
+          || participant.sector != currentData.sector) {
+          val newCurrentData = CurrentData(
+            participant.currentLap, participant.sector,
+            participant.currentTime, participant.currentSectorTime
+          )
+          Some(mergeHistory(history, newCurrentData))
+        } else {
+          None
+        }
+      case None =>
+        val participant = udpData.partcipants(history.viewedParticipantIndex)
+        val newCurrentData = CurrentData(
+          participant.currentLap, participant.sector,
+          participant.currentTime, participant.currentSectorTime
+        )
+        Some(mergeHistory(history, newCurrentData))
+    }
+  }
+
+  def createHistory(udpData: TimeStatsData, history: History): Option[History] = {
+    (history.currentData, history.currentLapData) match {
+      case (Some(currentData), Some(currentLapData)) =>
+        currentData.currentLap match {
+          case currentLap if currentLap == currentLapData.lap =>
+            currentData.sector match {
+              case 1 =>
+                val lapData = LapData(currentData.currentLap, None, None, None, None)
+                Some(mergeHistory(history, lapData))
+              case 2 =>
+                val stat = udpData.stats.participants(history.viewedParticipantIndex)
+                val lapData = LapData(currentData.currentLap, Some(stat.lastSectorTime), None, None, None)
+                Some(mergeHistory(history, lapData))
+              case 3 =>
+                val stat = udpData.stats.participants(history.viewedParticipantIndex)
+                val lapData = LapData(currentData.currentLap, currentLapData.sector1, Some(stat.lastSectorTime), None, None)
+                Some(mergeHistory(history, lapData))
+              case _ =>
+                logger.warn(s"Received unknown sector: ${currentData.sector}")
+                Some(createHistory(history.viewedParticipantIndex))
+            }
+          case currentLap if currentLap > currentLapData.lap =>
+            (currentLapData.sector1, currentLapData.sector2, currentLapData.sector3) match {
+              case (Some(_), Some(_), None) =>
+                val stat = udpData.stats.participants(history.viewedParticipantIndex)
+                val lapData = LapData(
+                  currentLapData.lap, currentLapData.sector1, currentLapData.sector2, Some(stat.lastSectorTime), Some(stat.lastLapTime)
+                )
+                Some(addHistory(history, lapData))
+              case (Some(_), None, None) =>
+                val stat = udpData.stats.participants(history.viewedParticipantIndex)
+                val lapData = LapData(
+                  currentLapData.lap, currentLapData.sector1, Some(stat.lastSectorTime), None, Some(stat.lastLapTime)
+                )
+                Some(addHistory(history, lapData))
+              case (None, None, None) =>
+                val stat = udpData.stats.participants(history.viewedParticipantIndex)
+                val lapData = LapData(
+                  currentLapData.lap, Some(stat.lastSectorTime), None, None, Some(stat.lastLapTime)
+                )
+                Some(addHistory(history, lapData))
+              case _ =>
+                Some(createHistory(history.viewedParticipantIndex))
+            }
+          case _ =>
+            Some(createHistory(history.viewedParticipantIndex))
+        }
+      case (Some(currentData), None) =>
+        currentData.sector match {
+          case 1 =>
+            val lapData = LapData(currentData.currentLap, None, None, None, None)
+            Some(mergeHistory(history, lapData))
+          case 2 =>
+            val stat = udpData.stats.participants(history.viewedParticipantIndex)
+            val lapData = LapData(currentData.currentLap, Some(stat.lastSectorTime), None, None, None)
+            Some(mergeHistory(history, lapData))
+          case 3 =>
+            val stat = udpData.stats.participants(history.viewedParticipantIndex)
+            val lapData = LapData(currentData.currentLap, None, Some(stat.lastSectorTime), None, None)
+            Some(mergeHistory(history, lapData))
+          case _ =>
+            logger.warn(s"Received unknown sector: ${currentData.sector}")
+            Some(createHistory(history.viewedParticipantIndex))
+        }
+      case _ => None
+    }
   }
 
   private def mergeHistory(history: History, currentData: CurrentData): History = {
@@ -148,118 +264,5 @@ final class TimeDetailsStorage(clientManager: ClientManager)
       None,
       history.lapDataList :+ currentLapData
     )
-  }
-
-  def receive(): Receive = {
-    case udpData: TelemetryData =>
-      context.become(processing(createHistory(udpData.participantinfo.viewedParticipantIndex)))
-    case _ => Unit
-  }
-
-  def processing(history: History): Receive = {
-    case udpData: TelemetryData if udpData.participantinfo.viewedParticipantIndex != history.viewedParticipantIndex =>
-      val nextHistory = createHistory(udpData.participantinfo.viewedParticipantIndex)
-      context.become(processing(nextHistory))
-    case udpData: TimingsData =>
-      history.currentData match {
-        case Some(currentData) =>
-          val participant = udpData.partcipants(history.viewedParticipantIndex)
-          if (participant.currentLap != currentData.currentLap
-            || participant.sector != currentData.sector) {
-            val newCurrentData = CurrentData(
-              participant.currentLap, participant.sector,
-              participant.currentTime, participant.currentSectorTime
-            )
-            val nextHistory = mergeHistory(history, newCurrentData)
-            context.become(processing(nextHistory))
-          }
-        case None =>
-          val participant = udpData.partcipants(history.viewedParticipantIndex)
-          val newCurrentData = CurrentData(
-            participant.currentLap, participant.sector,
-            participant.currentTime, participant.currentSectorTime
-          )
-          val nextHistory = mergeHistory(history, newCurrentData)
-          context.become(processing(nextHistory))
-      }
-    case udpData: TimeStatsData =>
-      (history.currentData, history.currentLapData) match {
-        case (Some(currentData), Some(currentLapData)) =>
-          currentData.currentLap match {
-            case currentLap if currentLap == currentLapData.lap =>
-              currentData.sector match {
-                case 1 =>
-                  val lapData = LapData(currentData.currentLap, None, None, None, None)
-                  val nextHistory = mergeHistory(history, lapData)
-                  context.become(processing(nextHistory))
-                case 2 =>
-                  val stat = udpData.stats.participants(history.viewedParticipantIndex)
-                  val lapData = LapData(currentData.currentLap, Some(stat.lastSectorTime), None, None, None)
-                  val nextHistory = mergeHistory(history, lapData)
-                  context.become(processing(nextHistory))
-                case 3 =>
-                  val stat = udpData.stats.participants(history.viewedParticipantIndex)
-                  val lapData = LapData(currentData.currentLap, currentLapData.sector1, Some(stat.lastSectorTime), None, None)
-                  val nextHistory = mergeHistory(history, lapData)
-                  context.become(processing(nextHistory))
-                case _ =>
-                  logger.warn(s"Received unknown sector: ${currentData.sector}")
-                  val nextHistory = createHistory(history.viewedParticipantIndex)
-                  context.become(processing(nextHistory))
-              }
-            case currentLap if currentLap > currentLapData.lap =>
-              (currentLapData.sector1, currentLapData.sector2, currentLapData.sector3) match {
-                case (Some(_), Some(_), None) =>
-                  val stat = udpData.stats.participants(history.viewedParticipantIndex)
-                  val lapData = LapData(
-                    currentLapData.lap, currentLapData.sector1, currentLapData.sector2, Some(stat.lastSectorTime), Some(stat.lastLapTime)
-                  )
-                  val nextHistory = addHistory(history, lapData)
-                  context.become(processing(nextHistory))
-                case (Some(_), None, None) =>
-                  val stat = udpData.stats.participants(history.viewedParticipantIndex)
-                  val lapData = LapData(
-                    currentLapData.lap, currentLapData.sector1, Some(stat.lastSectorTime), None, Some(stat.lastLapTime)
-                  )
-                  val nextHistory = addHistory(history, lapData)
-                  context.become(processing(nextHistory))
-                case (None, None, None) =>
-                  val stat = udpData.stats.participants(history.viewedParticipantIndex)
-                  val lapData = LapData(
-                    currentLapData.lap, Some(stat.lastSectorTime), None, None, Some(stat.lastLapTime)
-                  )
-                  val nextHistory = addHistory(history, lapData)
-                  context.become(processing(nextHistory))
-                case _ =>
-                  val nextHistory = createHistory(history.viewedParticipantIndex)
-                  context.become(processing(nextHistory))
-              }
-            case _ =>
-              val nextHistory = createHistory(history.viewedParticipantIndex)
-              context.become(processing(nextHistory))
-          }
-        case (Some(currentData), None) =>
-          currentData.sector match {
-            case 1 =>
-              val lapData = LapData(currentData.currentLap, None, None, None, None)
-              val nextHistory = mergeHistory(history, lapData)
-              context.become(processing(nextHistory))
-            case 2 =>
-              val stat = udpData.stats.participants(history.viewedParticipantIndex)
-              val lapData = LapData(currentData.currentLap, Some(stat.lastSectorTime), None, None, None)
-              val nextHistory = mergeHistory(history, lapData)
-              context.become(processing(nextHistory))
-            case 3 =>
-              val stat = udpData.stats.participants(history.viewedParticipantIndex)
-              val lapData = LapData(currentData.currentLap, None, Some(stat.lastSectorTime), None, None)
-              val nextHistory = mergeHistory(history, lapData)
-              context.become(processing(nextHistory))
-            case _ =>
-              logger.warn(s"Received unknown sector: ${currentData.sector}")
-              val nextHistory = createHistory(history.viewedParticipantIndex)
-              context.become(processing(nextHistory))
-          }
-        case _ => Unit
-      }
   }
 }
