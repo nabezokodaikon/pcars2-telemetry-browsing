f8224e550781f78e1f017108dedfbd5a50bc6f89
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-11-13 00:00:23 +0900

    Add time header component

--- public/src/contents/time/TimeContainer.jsx
+++ public/src/contents/time/TimeContainer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import createHeaderComponent from "./headerComponent.jsx"
+
 
 class TimeContent extends React.Component {
   constructor(props) {
@@ -30,6 +32,7 @@ class TimeContent extends React.Component {
   createTimeTable() {
     return (
     <div style={this.getTimeTableStyle()}>
+      {createHeaderComponent()}
     </div>
     );
   }
---
+++ public/src/contents/time/headerComponent.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import timeStyle from "./time.css";
+
+export default function createHeaderComponent() {
+  return (
+    <div className={timeStyle.headerColumn}>
+      <div className={timeStyle.headerCell}>
+        <div>
+          <span></span>
+        </div>
+      </div>
+      <div className={timeStyle.lapCountCell}>
+        <div>
+          <span>LAP</span>
+        </div>
+      </div>
+      <div className={timeStyle.sectorCell}>
+        <div>
+          <span>SECTOR 1</span>
+        </div>
+      </div>
+      <div className={timeStyle.sectorCell}>
+        <div>
+          <span>SECTOR 2</span>
+        </div>
+      </div>
+      <div className={timeStyle.sectorCell}>
+        <div>
+          <span>SECTOR 3</span>
+        </div>
+      </div>
+      <div className={timeStyle.lapTimeCell}>
+        <div>
+          <span>LAP TIME</span>
+        </div>
+      </div>
+    </div>
+  );
+}
