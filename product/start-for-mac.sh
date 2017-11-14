f26af1c0ea5a326ddb3387a236195c21d3b223a7
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-11-15 00:15:43 +0900

    Add ASCII art

--- product/start-for-mac.sh
+++ product/start-for-mac.sh
@@ -1,4 +1,10 @@
 #/bin/bash
+echo ' ____   ____                 ____  '  
+echo '|  _ \ / ___|__ _ _ __ ___  |___ \ '
+echo '| |_) | |   / _` | '\''__/ __|   __) |'
+echo '|  __/| |__| (_| | |  \__ \  / __/ '
+echo '|_|    \____\__,_|_|  |___/ |_____|'
+echo ''
 
 java \
   -Dlogback.configurationFile=./logback.xml \
--- product/start-for-windows.bat
+++ product/start-for-windows.bat
@@ -1,5 +1,12 @@
 @echo off
 
+echo "  ____   ____                 ____  "
+echo " |  _ \ / ___|__ _ _ __ ___  |___ \ "
+echo " | |_) | |   / _` | '__/ __|   __) |"
+echo " |  __/| |__| (_| | |  \__ \  / __/ "
+echo " |_|    \____\__,_|_|  |___/ |_____|"
+echo;
+
 java ^
 -Dlogback.configurationFile=.\logback.xml ^
 -Dconfig.file=.\application.conf ^
