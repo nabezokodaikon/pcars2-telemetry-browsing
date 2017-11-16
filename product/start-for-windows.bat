f9fbda1ff1140ebcaa7986255fd09e8ed9fff8fc
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-11-16 14:59:56 +0900

    Modify version to 0.1

--- product/start-for-windows.bat
+++ product/start-for-windows.bat
@@ -10,4 +10,4 @@ echo;
 java ^
 -Dlogback.configurationFile=.\logback.xml ^
 -Dconfig.file=.\application.conf ^
--jar pcars2-telemetry-browsing-assembly-0.0.1.jar
+-jar pcars2-telemetry-browsing-assembly-0.1.jar
