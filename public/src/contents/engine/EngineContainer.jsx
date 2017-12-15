d84594b86f2d2f39af00a07168e400164ebfdf42
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-12-16 00:19:12 +0900

    Modify EngineContent

--- public/src/contents/engine/EngineContainer.jsx
+++ public/src/contents/engine/EngineContainer.jsx
@@ -11,133 +11,282 @@ import shareStyle from "../../share/largeContent.css";
 import engineIcon from "../../image/engine.png";
 import oilIcon from "../../image/oil.png";
 import waterIcon from "../../image/water.png";
+import fuelIcon from "../../image/fuel.png";
 import style from "./engine.css";
+
+const Engine = props => {
+  const tyre3 = props.tyre3;
+
+  return (
+    <div className={style.engine}>
+
+      <div className={style.icon}>
+        <img src={engineIcon} />
+      </div>
+
+      <div className={style.valueRecords}>
+
+        <div className={style.valueRecord}>
+          <div className={style.value}>
+            <span>{tyre3.enginePower}</span>
+          </div>
+          <div className={style.unit}>
+            <span>HP</span>
+          </div>
+        </div>
+
+        <div className={style.valueRecord}>
+          <div className={style.value}>
+            <span>{tyre3.engineTorque}</span>
+          </div>
+          <div className={style.unit}>
+            <span>NM</span>
+          </div>
+        </div>
+
+        <div className={style.valueRecord}>
+          <div className={style.value}>
+            <span>{tyre3.engineSpeed}</span>
+          </div>
+          <div className={style.unit}>
+            <span>rad/s</span>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+const ValueTableHeader = props => {
+  return (
+    <div className={style.header}>
+      <div>
+      </div>
+      <div>
+        <span>CURRENT</span>
+      </div>
+      <div>
+        <span>MIN</span>
+      </div>
+      <div>
+        <span>MAX</span>
+      </div>
+    </div>
+  );
+}
+
+const Oil = props => {
+  const carState = props.carState;
+  const engine = props.engine;
+  const isCelsius = props.isCelsius;
+
+  return (
+    <div className={style.oil}>
+
+      <div className={style.icon}>
+        <img src={oilIcon} />
+      </div>
+
+      <div className={style.valueRecords}>
+
+        <div className={style.valueRecord}>
+          <div>
+            <div className={style.value}>
+              <span>{getTemp(carState.oilTempCelsius, isCelsius)}</span>
+            </div>
+            <div className={style.unit}>
+              <span>{getTempUnit(isCelsius)}</span>
+            </div>
+          </div>
+          <div>
+            <div className={style.value}>
+              <span>{getTemp(engine.minOilTempCelsius, isCelsius)}</span>
+            </div>
+            <div className={style.unit}>
+              <span>{getTempUnit(isCelsius)}</span>
+            </div>
+          </div>
+          <div>
+            <div className={style.value}>
+              <span>{getTemp(engine.maxOilTempCelsius, isCelsius)}</span>
+            </div>
+            <div className={style.unit}>
+              <span>{getTempUnit(isCelsius)}</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={style.valueRecord}>
+          <div>
+            <div className={style.value}>
+              <span>{carState.oilPressureKPa}</span>
+            </div>
+            <div className={style.unit}>
+              <span>kPa</span>
+            </div>
+          </div>
+          <div>
+            <div className={style.value}>
+              <span>{engine.minOilPressureKPa}</span>
+            </div>
+            <div className={style.unit}>
+              <span>kPa</span>
+            </div>
+          </div>
+          <div>
+            <div className={style.value}>
+              <span>{engine.maxOilPressureKPa}</span>
+            </div>
+            <div className={style.unit}>
+              <span>kPa</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+const Water = props => {
+  const carState = props.carState;
+  const engine = props.engine;
+  const isCelsius = props.isCelsius;
+
+  return (
+    <div className={style.water}>
+
+      <div className={style.icon}>
+        <img src={waterIcon} />
+      </div>
+
+      <div className={style.valueRecords}>
+
+        <div className={style.valueRecord}>
+          <div>
+            <div className={style.value}>
+              <span>{getTemp(carState.waterTempCelsius, isCelsius)}</span>
+            </div>
+            <div className={style.unit}>
+              <span>{getTempUnit(isCelsius)}</span>
+            </div>
+          </div>
+          <div>
+            <div className={style.value}>
+              <span>{getTemp(engine.minWaterTempCelsius, isCelsius)}</span>
+            </div>
+            <div className={style.unit}>
+              <span>{getTempUnit(isCelsius)}</span>
+            </div>
+          </div>
+          <div>
+            <div className={style.value}>
+              <span>{getTemp(engine.maxWaterTempCelsius, isCelsius)}</span>
+            </div>
+            <div className={style.unit}>
+              <span>{getTempUnit(isCelsius)}</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={style.valueRecord}>
+          <div>
+            <div className={style.value}>
+              <span>{carState.waterPressureKPa}</span>
+            </div>
+            <div className={style.unit}>
+              <span>kPa</span>
+            </div>
+          </div>
+          <div>
+            <div className={style.value}>
+              <span>{engine.minWaterPressureKPa}</span>
+            </div>
+            <div className={style.unit}>
+              <span>kPa</span>
+            </div>
+          </div>
+          <div>
+            <div className={style.value}>
+              <span>{engine.maxWaterPressureKPa}</span>
+            </div>
+            <div className={style.unit}>
+              <span>kPa</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+const Fuel = props => {
+  const carState = props.carState;
+  const engine = props.engine;
+
+  return (
+    <div className={style.fuel}>
+
+      <div className={style.icon}>
+        <img src={fuelIcon} />
+      </div>
+
+      <div className={style.valueRecords}>
+
+        <div className={style.valueRecord}>
+          <div>
+            <div className={style.value}>
+              <span>{carState.fuelPressureKPa}</span>
+            </div>
+            <div className={style.unit}>
+              <span>kPa</span>
+            </div>
+          </div>
+          <div>
+            <div className={style.value}>
+              <span>{engine.minFuelPressureKPa}</span>
+            </div>
+            <div className={style.unit}>
+              <span>kPa</span>
+            </div>
+          </div>
+          <div>
+            <div className={style.value}>
+              <span>{engine.maxFuelPressureKPa}</span>
+            </div>
+            <div className={style.unit}>
+              <span>kPa</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
 
 class EngineContent extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  createEngine() {
+  render() {
     const props = this.props;
     const telemetryData = props.telemetryData;
-    if (!isJson(telemetryData)) {
+    const telemetrySummary = props.telemetrySummary;
+    if (!isJson(telemetryData) || !isJson(telemetrySummary)) {
       return <div></div>;
     }
 
     const tyre3 = telemetryData.tyre3;
-
-    return (
-      <div className={style.engine}>
-        <div className={style.icon}>
-          <img src={engineIcon} />
-        </div>
-        <div className={style.values}>
-          <div className={style.valueBox}>
-            <div className={shareStyle.value}>
-              <span>{tyre3.enginePower}</span>
-            </div>
-            <div className={[style.unit, shareStyle.value].join(" ")}>
-              <span>HP</span>
-            </div>
-          </div>
-          <div className={style.valueBox}>
-            <div className={shareStyle.value}>
-              <span>{tyre3.engineTorque}</span>
-            </div>
-            <div className={[style.unit, shareStyle.value].join(" ")}>
-              <span>NM</span>
-            </div>
-          </div>
-          <div className={style.valueBox}>
-            <div className={shareStyle.value}>
-              <span>{tyre3.engineSpeed}</span>
-            </div>
-            <div className={[style.unit, shareStyle.value].join(" ")}>
-              <span>rad/s</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  createOil() {
-    const props = this.props;
-    const telemetryData = props.telemetryData;
-    if (!isJson(telemetryData)) {
-      return <div></div>;
-    }
-
     const carState = telemetryData.carState;
-    const isCelsius = props.isCelsius;
-
-    return (
-      <div className={style.oil}>
-        <div className={style.icon}>
-          <img src={oilIcon} />
-        </div>
-        <div className={style.values}>
-          <div className={style.valueBox}>
-            <div className={shareStyle.value}>
-              <span>{getTemp(carState.oilTempCelsius, isCelsius)}</span>
-            </div>
-            <div className={[style.unit, shareStyle.value].join(" ")}>
-              <span>{getTempUnit(isCelsius)}</span>
-            </div>
-          </div>
-          <div className={style.valueBox}>
-            <div className={shareStyle.value}>
-              <span>{carState.oilPressureKPa}</span>
-            </div>
-            <div className={[style.unit, shareStyle.value].join(" ")}>
-              <span>kPa</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  createWater() {
-    const props = this.props;
-    const telemetryData = props.telemetryData;
-    if (!isJson(telemetryData)) {
-      return <div></div>;
-    }
-
-    const carState = telemetryData.carState;
-    const isCelsius = props.isCelsius;
-
-    return (
-      <div className={style.water}>
-        <div className={style.icon}>
-          <img src={waterIcon} />
-        </div>
-        <div className={style.values}>
-          <div className={style.valueBox}>
-            <div className={shareStyle.value}>
-              <span>{getTemp(carState.waterTempCelsius, isCelsius)}</span>
-            </div>
-            <div className={[style.unit, shareStyle.value].join(" ")}>
-              <span>{getTempUnit(isCelsius)}</span>
-            </div>
-          </div>
-          <div className={style.valueBox}>
-            <div className={shareStyle.value}>
-              <span>{carState.waterPressureKpa}</span>
-            </div>
-            <div className={[style.unit, shareStyle.value].join(" ")}>
-              <span>kPa</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  render() {
-    const props = this.props;
+    const engine = telemetrySummary.engine;
 
     return (
       <div className={shareStyle.contents} onClick={props.onContentClick}>
@@ -146,9 +295,13 @@ class EngineContent extends React.Component {
             <LargeGearComponent isMeter={props.isMeter} telemetryData={props.telemetryData} />
           </div>
           <div className={shareStyle.rightContents}>
-            {this.createEngine()}
-            {this.createOil()}
-            {this.createWater()}
+            <Engine tyre3={tyre3} />
+            <div className={style.valueTable}>
+              <ValueTableHeader />
+              <Oil carState={carState} engine={engine} isCelsius={props.isCelsius} />
+              <Water carState={carState} engine={engine} isCelsius={props.isCelsius} />
+              <Fuel carState={carState} engine={engine} />
+            </div>
           </div>
         </div>
         <div className={shareStyle.bottomContents}>
@@ -164,6 +317,7 @@ EngineContent.propTypes = {
   isCelsius: PropTypes.bool.isRequired,
   telemetryData: PropTypes.object.isRequired,
   fuelData: PropTypes.object.isRequired,
+  telemetrySummary: PropTypes.object.isRequired,
   onContentClick: PropTypes.func.isRequired
 };
 
@@ -173,7 +327,8 @@ const mapStateToProps = state => {
     isMeter: state.options.isMeter,
     isCelsius: state.options.isCelsius,
     telemetryData: data.telemetryData,
-    fuelData: data.fuelData
+    fuelData: data.fuelData,
+    telemetrySummary: data.telemetrySummary
   };
 };
 
