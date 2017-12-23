77ad8f5a815dcc7157211525636cdbba0165b02f
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-12-23 21:16:28 +0900

    Add ButtonBox logic

--- public/src/contents/buttonBox/ButtonBoxContainer.jsx
+++ public/src/contents/buttonBox/ButtonBoxContainer.jsx
@@ -4,16 +4,20 @@ import { connect } from "react-redux";
 import SmallSpeedComponent from "../../share/SmallSpeedComponent.jsx";
 import SmallFuelComponent from "../../share/SmallFuelComponent.jsx";
 import shareStyle from "../../share/smallContent.css";
+import style from "./buttonBox.css";
+import buttonStyle from "./button.css";
 import Button from "./Button.jsx";
+import ButtonWithDescription from "./ButtonWithDescription.jsx";
 
 class ButtonBoxContent extends React.Component {
   constructor(props) {
     super(props);
 
-    this.requestButtonAction = this.requestButtonAction.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleCustomButtonClick = this.handleCustomButtonClick.bind(this);
   }
 
-  requestButtonAction(index) {
+  handleButtonClick(index) {
     const url = "buttonBox/action";
     const json = {
       index: index
@@ -30,21 +34,77 @@ class ButtonBoxContent extends React.Component {
     .catch(error => console.log(error.message));
   }
 
-  handleButtonClick(index) {
-    this.props.onButtonClick(index);
+  handleCustomButtonClick() {
+    // TODO
+    console.log("custom button clicked.");
   }
 
   render() {
     const props = this.props;
     const telemetryData = props.telemetryData;
 
+    const red = buttonStyle.red;
+    const yellow = buttonStyle.yellow;
+    const green = buttonStyle.green;
+    const blue = buttonStyle.blue;
+
     return (
       <div className={shareStyle.contents}>
-        <div className={shareStyle.topContents}>
-          <button onClick={() => this.requestButtonAction(0)}>
-            <span>Test Button</span>
-          </button>
-          <Button />
+        <div className={style.topContents}>
+
+          <div className={style.leftButtons}>
+
+            <div className={style.leftTopButtons}>
+              <div>
+                <div className={style.crossButtons}>
+                  <Button index={1} onClick={this.handleButtonClick} color={red} />
+                </div>
+                <div className={style.crossButtons}>
+                  <Button index={0} onClick={this.handleButtonClick} color={red} />
+                  <Button index={2} onClick={this.handleButtonClick} color={red} />
+                  <Button index={4} onClick={this.handleButtonClick} color={red} />
+                </div>
+                <div className={style.crossButtons}>
+                  <Button index={3} onClick={this.handleButtonClick} color={red} />
+                </div>
+              </div>
+            </div>
+
+            <div className={style.leftBottomButtons}>
+              <div className={style.customButton} onClick={this.handleCustomButtonClick}>
+                <span>CUSTOM</span>
+              </div>
+            </div>
+
+          </div> 
+
+          <div className={style.rightButtons}>
+            <div className={style.rightColumn}>
+              <ButtonWithDescription index={5} onClick={this.handleButtonClick} color={red} description={"SCROLL1"} />
+              <ButtonWithDescription index={6} onClick={this.handleButtonClick} color={red} description={"SCROLL1"} />
+              <ButtonWithDescription index={7} onClick={this.handleButtonClick} color={red} description={"SCROLL1"} />
+              <ButtonWithDescription index={8} onClick={this.handleButtonClick} color={red} description={"SCROLL1"} />
+            </div>
+            <div className={style.rightColumn}>
+              <ButtonWithDescription index={9} onClick={this.handleButtonClick} color={yellow} description={"SCROLL1"} />
+              <ButtonWithDescription index={10} onClick={this.handleButtonClick} color={yellow} description={"SCROLL1"} />
+              <ButtonWithDescription index={11} onClick={this.handleButtonClick} color={yellow} description={"SCROLL1"} />
+              <ButtonWithDescription index={12} onClick={this.handleButtonClick} color={yellow} description={"SCROLL1"} />
+            </div>
+            <div className={style.rightColumn}>
+              <ButtonWithDescription index={13} onClick={this.handleButtonClick} color={green} description={"SCROLL1"} />
+              <ButtonWithDescription index={14} onClick={this.handleButtonClick} color={green} description={"SCROLL1"} />
+              <ButtonWithDescription index={15} onClick={this.handleButtonClick} color={green} description={"SCROLL1"} />
+              <ButtonWithDescription index={16} onClick={this.handleButtonClick} color={green} description={"SCROLL1"} />
+            </div>
+            <div className={style.rightColumn}>
+              <ButtonWithDescription index={17} onClick={this.handleButtonClick} color={blue} description={"SCROLL1"} />
+              <ButtonWithDescription index={18} onClick={this.handleButtonClick} color={blue} description={"SCROLL1"} />
+              <ButtonWithDescription index={19} onClick={this.handleButtonClick} color={blue} description={"SCROLL1"} />
+              <ButtonWithDescription index={20} onClick={this.handleButtonClick} color={blue} description={"SCROLL1"} />
+            </div>
+          </div>
+
         </div>
         <div className={shareStyle.bottomContents} onClick={props.onContentClick}>
           <SmallSpeedComponent isMeter={props.isMeter} telemetryData={telemetryData} />
