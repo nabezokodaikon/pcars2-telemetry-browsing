930fcfed19dbd029494f0a909d8864b581523cf4
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-12-25 20:38:39 +0900

    Remove small contents on ButtonBox

--- public/src/contents/buttonBox/ButtonBoxContainer.jsx
+++ public/src/contents/buttonBox/ButtonBoxContainer.jsx
@@ -6,9 +6,6 @@ import {
   requestAllButtonBoxMappings
 } from "../../appActionCreators.js";
 import * as contentNames from "../../share/contentNames.js";
-import SmallSpeedComponent from "../../share/SmallSpeedComponent.jsx";
-import SmallFuelComponent from "../../share/SmallFuelComponent.jsx";
-import shareStyle from "../../share/smallContent.css";
 import style from "./buttonBox.css";
 import buttonStyle from "./button.css";
 import Button from "./Button.jsx";
@@ -47,88 +44,79 @@ class ButtonBoxContent extends React.Component {
 
   render() {
     const props = this.props;
-    const telemetryData = props.telemetryData;
-
-    const red = buttonStyle.red;
-    const yellow = buttonStyle.yellow;
-    const green = buttonStyle.green;
-    const blue = buttonStyle.blue;
 
     const mappings = props.mappings;
     if (mappings.length < 1) {
       return <div></div>;
     }
 
+    const red = buttonStyle.red;
+    const yellow = buttonStyle.yellow;
+    const green = buttonStyle.green;
+    const blue = buttonStyle.blue;
+
     return (
-      <div className={shareStyle.contents}>
-        <div className={style.topContents}>
+      <div className={style.topContents}>
 
-          <div className={style.leftButtons}>
+        <div className={style.leftButtons}>
 
-            <div className={style.leftTopButtons}>
-              <div>
-                <div className={style.crossButtons}>
-                  <Button index={1} onClick={this.handleButtonClick} color={red} />
-                </div>
-                <div className={style.crossButtons}>
-                  <Button index={0} onClick={this.handleButtonClick} color={red} />
-                  <Button index={2} onClick={this.handleButtonClick} color={red} />
-                  <Button index={4} onClick={this.handleButtonClick} color={red} />
-                </div>
-                <div className={style.crossButtons}>
-                  <Button index={3} onClick={this.handleButtonClick} color={red} />
-                </div>
+          <div className={style.leftTopButtons}>
+            <div>
+              <div className={style.crossButtons}>
+                <Button index={1} onClick={this.handleButtonClick} color={red} />
               </div>
-            </div>
-
-            <div className={style.leftBottomButtons}>
-              <div className={style.customizeButton} onClick={this.handleCustomizeButtonClick}>
-                <span>CUSTOMIZE</span>
+              <div className={style.crossButtons}>
+                <Button index={0} onClick={this.handleButtonClick} color={red} />
+                <Button index={2} onClick={this.handleButtonClick} color={red} />
+                <Button index={4} onClick={this.handleButtonClick} color={red} />
               </div>
-            </div>
-
-          </div> 
-
-          <div className={style.rightButtons}>
-            <div className={style.rightColumn}>
-              <ButtonWithDescription index={5} onClick={this.handleButtonClick} color={red} description={mappings[5].description} />
-              <ButtonWithDescription index={6} onClick={this.handleButtonClick} color={red} description={mappings[6].description} />
-              <ButtonWithDescription index={7} onClick={this.handleButtonClick} color={red} description={mappings[7].description} />
-              <ButtonWithDescription index={8} onClick={this.handleButtonClick} color={red} description={mappings[8].description} />
-            </div>
-            <div className={style.rightColumn}>
-              <ButtonWithDescription index={9} onClick={this.handleButtonClick} color={yellow} description={mappings[9].description} />
-              <ButtonWithDescription index={10} onClick={this.handleButtonClick} color={yellow} description={mappings[10].description} />
-              <ButtonWithDescription index={11} onClick={this.handleButtonClick} color={yellow} description={mappings[11].description} />
-              <ButtonWithDescription index={12} onClick={this.handleButtonClick} color={yellow} description={mappings[12].description} />
-            </div>
-            <div className={style.rightColumn}>
-              <ButtonWithDescription index={13} onClick={this.handleButtonClick} color={green} description={mappings[13].description} />
-              <ButtonWithDescription index={14} onClick={this.handleButtonClick} color={green} description={mappings[14].description} />
-              <ButtonWithDescription index={15} onClick={this.handleButtonClick} color={green} description={mappings[15].description} />
-              <ButtonWithDescription index={16} onClick={this.handleButtonClick} color={green} description={mappings[16].description} />
-            </div>
-            <div className={style.rightColumn}>
-              <ButtonWithDescription index={17} onClick={this.handleButtonClick} color={blue} description={mappings[17].description} />
-              <ButtonWithDescription index={18} onClick={this.handleButtonClick} color={blue} description={mappings[18].description} />
-              <ButtonWithDescription index={19} onClick={this.handleButtonClick} color={blue} description={mappings[19].description} />
-              <ButtonWithDescription index={20} onClick={this.handleButtonClick} color={blue} description={mappings[20].description} />
+              <div className={style.crossButtons}>
+                <Button index={3} onClick={this.handleButtonClick} color={red} />
+              </div>
             </div>
           </div>
 
+          <div className={style.leftBottomButtons}>
+            <div className={style.customizeButton} onClick={this.handleCustomizeButtonClick}>
+              <span>CUSTOMIZE</span>
+            </div>
+          </div>
+
+        </div> 
+
+        <div className={style.rightButtons}>
+          <div className={style.rightColumn}>
+            <ButtonWithDescription index={5} onClick={this.handleButtonClick} color={red} description={mappings[5].description} />
+            <ButtonWithDescription index={6} onClick={this.handleButtonClick} color={red} description={mappings[6].description} />
+            <ButtonWithDescription index={7} onClick={this.handleButtonClick} color={red} description={mappings[7].description} />
+            <ButtonWithDescription index={8} onClick={this.handleButtonClick} color={red} description={mappings[8].description} />
+          </div>
+          <div className={style.rightColumn}>
+            <ButtonWithDescription index={9} onClick={this.handleButtonClick} color={yellow} description={mappings[9].description} />
+            <ButtonWithDescription index={10} onClick={this.handleButtonClick} color={yellow} description={mappings[10].description} />
+            <ButtonWithDescription index={11} onClick={this.handleButtonClick} color={yellow} description={mappings[11].description} />
+            <ButtonWithDescription index={12} onClick={this.handleButtonClick} color={yellow} description={mappings[12].description} />
+          </div>
+          <div className={style.rightColumn}>
+            <ButtonWithDescription index={13} onClick={this.handleButtonClick} color={green} description={mappings[13].description} />
+            <ButtonWithDescription index={14} onClick={this.handleButtonClick} color={green} description={mappings[14].description} />
+            <ButtonWithDescription index={15} onClick={this.handleButtonClick} color={green} description={mappings[15].description} />
+            <ButtonWithDescription index={16} onClick={this.handleButtonClick} color={green} description={mappings[16].description} />
+          </div>
+          <div className={style.rightColumn}>
+            <ButtonWithDescription index={17} onClick={this.handleButtonClick} color={blue} description={mappings[17].description} />
+            <ButtonWithDescription index={18} onClick={this.handleButtonClick} color={blue} description={mappings[18].description} />
+            <ButtonWithDescription index={19} onClick={this.handleButtonClick} color={blue} description={mappings[19].description} />
+            <ButtonWithDescription index={20} onClick={this.handleButtonClick} color={blue} description={mappings[20].description} />
+          </div>
         </div>
-        <div className={shareStyle.bottomContents} onClick={props.onContentClick}>
-          <SmallSpeedComponent isMeter={props.isMeter} telemetryData={telemetryData} />
-          <SmallFuelComponent telemetryData={telemetryData} />
-        </div>
+
       </div>
     );
   }
 }
 
 ButtonBoxContent.propTypes = {
-  isMeter: PropTypes.bool.isRequired,
-  telemetryData: PropTypes.object.isRequired,
   mappings: PropTypes.array.isRequired,
   onRequestAllButtonBoxMappings: PropTypes.func.isRequired,
   onCustomizeButtonClick: PropTypes.func.isRequired
